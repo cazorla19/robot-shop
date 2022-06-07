49f920827e16b18d532e0d7c953e247a5807e82e
cazorla19 <michael.chinkov@gmail.com>
2022-06-07 14:28:17 +0200

    remove instana dep from js services

--- cart/server.js
+++ cart/server.js
@@ -1,12 +1,3 @@
-const instana = require('@instana/collector');
-// init tracing
-// MUST be done before loading anything else!
-instana({
-    tracing: {
-        enabled: true
-    }
-});
-
 const redis = require('redis');
 const request = require('request');
 const bodyParser = require('body-parser');
@@ -56,8 +47,6 @@ app.use((req, res, next) => {
         "us-east1",
         "us-west1"
     ];
-    let span = instana.currentSpan();
-    span.annotate('custom.sdk.tags.datacenter', dcs[Math.floor(Math.random() * dcs.length)]);
 
     next();
 });
